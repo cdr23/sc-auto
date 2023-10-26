287bf240f411a49544cf86b720ff514d3e3831a9
cdr23 <142754652+cdr23@users.noreply.github.com>
2023-10-27 02:14:18 +0700

    Add files via upload

--- update/menu-backup.sh
+++ update/menu-backup.sh
@@ -1,96 +1,87 @@
-#!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/aixxy7/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m" 
-COLOR1="$(cat /etc/aixxy7/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/aixxy7/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
-###########- END COLOR CODE -##########
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/cdr23/sc-auto/main/permission/ipmini > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/cdr23/sc-auto/main/permission/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/cdr23/sc-auto/main/permission/ipmini | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
 clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}             • BACKUP PANEL MENU •             ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e " $COLOR1┌───────────────────────────────────────────────┐${NC}"
-echo -e " $COLOR1│$NC   ${COLOR1}[01]${NC} • BACKUP VPS      ${COLOR1}[03]${NC} • BACKUP API    $COLOR1│$NC"
-echo -e " $COLOR1│$NC   ${COLOR1}[02]${NC} • RESTORE VPS     ${COLOR1}[04]${NC} • RESET API     $COLOR1│$NC"
-echo -e " $COLOR1│$NC                                               $COLOR1│$NC"
-echo -e " $COLOR1│$NC   ${COLOR1}[00]${NC} • GO BACK${NC}                              $COLOR1│$NC"
-echo -e " $COLOR1└───────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}                • Premium Auto Script •          $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e ""
-read -p " Select menu :  "  opt
-echo -e ""
-case $opt in
-01 | 1) clear ; backup ;;
-02 | 2) clear ; restore ;;
-03 | 3) clear ; menu3 ;;
-04 | 4) clear ; menu4 ;;
-00 | 0) clear ; menu ;;
-*) clear ; menu-backup ;;
-esac
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
 
-       
+
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo -e
+echo -e "${BlueCyan}              Menu Backup                   "
+echo -e
+echo -e "            t.me/RJ-VPN"
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo
+echo -e "${BlueCyan}Pilih Nomor: "
+echo
+echo "1).Backup"
+echo "2).Restore"
+echo "3).strt"
+echo "4).limit speed"
+echo "5).autobackup"
+echo
+echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo -e "${BlueCyan}"
+read -p    "Pilih Nomor └╼>>>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+figlet "backup" | lolcat
+backup
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RJ-VPN"
+echo
+echo "--------------------------------------------------------"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+figlet "Restore" | lolcat
+restore
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RJ-VPN"
+echo
+echo "--------------------------------------------------------"
+fi
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "strt"
+strt
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RJ-VPN"
+echo
+echo "--------------------------------------------------------"
+fi
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+limitspeed
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RJ-VPN"
+echo
+echo "--------------------------------------------------------"
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+autobackup
+echo "--------------------------------------------------------"
+echo
+echo "Terimakasih sudah menggunakan layanan script RJ-VPN"
+echo
+echo "--------------------------------------------------------"
+fi
